3a70a8e3ce9c772c9c90ee1cff364eee05b13ff1
MadalenaCampos <madacampos02@gmail.com>
2022-04-29 09:47:54 -0300

    criacao dos models

---
+++ src/app/conversor/models/conversao-response.model.ts
@@ -0,0 +1,11 @@
+export class ConversaoResponse {
+  constructor(public base?: string, public date?: string, public rates?: any) {}
+}
+
+// {
+//   "base": "USD",
+//   "date": "2020-06-09",
+//   "rates": {
+//     "BRL": 3.818
+//   }
+// }
